aa23eaf23aac9f5f94e0057c83f57250efc93800
Brandie Lucano <blucano.dev@gmail.com>
2023-11-28 17:20:52 -0800

    wordfinder works

--- wordfinder.py
+++ wordfinder.py
@@ -1,26 +1,27 @@
 import random
 
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary.
-   # >>> wf = WordFinder("/usr/share/dict/words")
-    >>> wf = WordFinder("words.txt")
-    3 words read
+#    # >>> wf = WordFinder("/usr/share/dict/words")
+#     >>> wf = WordFinder("words.txt")
+#     3 words read
 
-    >>> wf.random()
-    'cat'
+#     >>> wf.random()
+#     'cat'
 
-    >>> wf.random()
-    'cat'
+#     >>> wf.random()
+#     'cat'
 
-    >>> wf.random()
-    'porcupine'
+#     >>> wf.random()
+#     'porcupine'
 
-    >>> wf.random()
-    'dog'
+#     >>> wf.random()
+#     'dog'
     """
 
-    #instantiated with a path to a file on disk that contains words, one word per line
-    def __init__ (self, path):
+    # instantiated with a path to a file on disk that contains words, one word per line
+    def __init__(self, path):
         """ Read file and make a list of words.
           Output number of words in the file"""
 
@@ -33,7 +34,7 @@ class WordFinder:
 
     def get_words_from_file(self, txt_file):
         """Accepts file. Returns list of lines"""
-        word_list=[]
+        word_list = []
 
         for line in txt_file:
             word_list.append(line)
@@ -52,35 +53,49 @@ class WordFinder:
         return clean_list
 
 
-#need a new class that ignores blank lines and lines that start with #
+# need a new class that ignores blank lines and lines that start with #
 class SpecialWordFinder(WordFinder):
     """Extends word finder to ignore blank lines and lines that start with #
     >>> wf = SpecialWordFinder("words.txt")
     9 words read
 
     >>> wf.random()
-    'cat'
+
 
     >>> wf.random()
-    'cat'
+
 
     >>> wf.random()
-    'porcupine'
+
 
     >>> wf.random()
-    'dog'
+
     """
 
     def __init__(self, path):
         super().__init__(path)
+        print(self.word_list)
         self.word_list = self.remove_blanks_and_hashes(self.word_list)
 
-    #we can reference the existing self.word_list
-    #check if first char is space or hash and pop if so
+    # we can reference the existing self.word_list
+    # check if first char is space or hash and pop if so
 
     def remove_blanks_and_hashes(self, word_list):
+        """Accepts word list, skips blanks and comments.
+        Returns clean word list"""
+
         clean_list = []
+
         for line in word_list:
-            if not (line.startswith(' ') and line.startswith('#')):
+            if len(line) > 1 and not line.startswith('#'):
                 clean_list.append(line)
+        print(clean_list)
         return clean_list
+
+        # for line in word_list:
+        #     clean_list.append(line)
+        #     # print(clean_list)
+        #     if line.startswith("#") or len(line) < 1:
+        #         clean_list.pop()
+        # print(clean_list)
+        # return clean_list
